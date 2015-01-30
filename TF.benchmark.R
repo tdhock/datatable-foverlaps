1eeb15ead101cd0182417816652b0ad8e9cf8c56
Toby Dylan Hocking <tdhock5@gmail.com>
2015-01-30 10:19:52 -0500

    read.bench compares fread to read.table with tricks

---
+++ TF.benchmark.R
@@ -0,0 +1,155 @@
+works_with_R("3.1.2",
+             dplyr="0.4.0",
+             microbenchmark="1.3.0",
+             "Rdatatable/data.table@84ba1151299ba49e833e68a2436630216b653306")
+
+downloads <- "http://tare.medisin.ntnu.no/chipseqbenchmark/downloads/"
+
+colClasses <-
+  c("chrom"="factor",
+    "chromStart"="integer",
+    "chromEnd"="integer",
+    "count"="integer")
+
+read.bench <- function(filename){
+  times <- microbenchmark(fread={
+    dt <- fread(filename)
+    setnames(dt, names(colClasses))
+    setkey(dt, chrom, chromStart, chromEnd)
+  }, read.table={
+    df <-
+      read.table(filename,
+                 colClasses = as.character(colClasses),
+                 nrows = wc(filename))
+  }, times=2)
+  stopifnot(nrow(dt) == nrow(df))
+  list(times=times, fread=dt, read.table=df)
+}
+
+site.list <- region.list <- coverage.list <- list()
+for(tf.name in c("max", "nrsf", "srf")){
+
+  ## First read manually annotated peak regions.
+  ann.file <-
+    paste0("Manually_classified_peaks/classified_peaks_", tf.name, ".txt")
+  ann.url <- paste0(downloads, ann.file)
+  nrsf.peaks <- read.table(ann.url)
+  bg.names <- if(tf.name == "max"){
+    c("background", "balanced", "replicate")
+  }else{
+    c("background1",
+      "background2",
+      "replicate",
+      "subset")    
+  }
+  names(nrsf.peaks) <-
+    c("chrom",
+      "chromStart",
+      "chromEnd",
+      "bases",
+      "region.index",
+      "initial",
+      bg.names,
+      "overall")
+
+  ## Also read TF binding site annotations.
+  sites.file <-
+    paste0("Manually_classified_sites/classified_sites_", tf.name, ".txt")
+  sites.url <- paste0(downloads, sites.file)
+  nrsf.sites <- read.table(sites.url)
+  names(nrsf.sites) <-
+    c("chrom",
+      "chromStart",
+      "chromEnd",
+      "pwm.score",
+      "tags.per.bp.within.site.sample",
+      if(tf.name != "max")"tags.per.bp.within.site.replicate" else NULL,
+      "region.index",
+      "classification")
+
+  ann.code <-
+    c("-1"="noPeaks",
+      "0"="ambiguous",
+      "1"="onePeak")
+
+  pos.neg <- data.table(tf.name, nrsf.peaks) %>%
+    mutate(annotation=ann.code[as.character(overall)])
+
+  ## Windows to show.
+  window.cols <- c("region.index", "chrom", "chromStart", "chromEnd")
+  sites.and.peaks <-
+    rbind(data.table(nrsf.sites[, window.cols], what="site"),
+          data.table(nrsf.peaks[, window.cols], what="region"))
+  expand.bases <- 3000
+  windows <- sites.and.peaks %>%
+    group_by(region.index, chrom) %>%
+      summarise(chromStart=min(chromStart)-expand.bases,
+                chromEnd=max(chromEnd)+expand.bases,
+                sites=sum(what=="site"),
+                regions=sum(what=="region"))
+  setkey(windows, chrom, chromStart, chromEnd)
+
+  ## Database of visual annotations.
+  pos.neg.regions <- pos.neg %>%
+    select(tf.name, region.index, chrom, chromStart, chromEnd, annotation)
+
+  tf.dir <- file.path("/home/thocking/genomecov", tf.name)
+  tf.files <- Sys.glob(file.path(tf.dir, "*.bedGraph"))
+  bg.files <- sub(tf.name, "backgr", tf.files)
+  bedgraph.files <- c(tf.files, bg.files[file.exists(bg.files)])
+  sample.list <- list()
+  for(bg.file.i in seq_along(bedgraph.files)){
+    bg.file <- bedgraph.files[[bg.file.i]]
+    sample.id <- sub(".bedGraph", "", basename(bg.file))
+    experiment <- basename(dirname(bg.file))
+    cat(sprintf("%4d / %4d %s %s\n",
+                bg.file.i, length(bedgraph.files),
+                sample.id, experiment))
+
+    bg.list <- read.bench(bg.file)
+
+    full.strand.list <- list(both=bg.list$fread)
+    full.strand.times <-
+      list(both=data.table(strand="both", bg.list$times))
+    for(strand in c("+", "-")){
+      plus.file <- paste0(bg.file, strand, "strand")
+      strand.list <- read.bench(plus.file)
+      full.strand.list[[strand]] <- strand.list$fread
+      full.strand.times[[strand]] <-
+        data.table(strand, strand.list$times)
+    }
+
+    window.strand.list <- list()
+    for(strand in names(full.strand.list)){
+      one.strand <- full.strand.list[[strand]]
+      one.join <- foverlaps(one.strand, windows) %>%
+        filter(!is.na(chromStart))
+      join.summary <- one.join %>%
+        group_by(region.index) %>%
+        summarise(segs=n())
+      stopifnot(join.summary$segs > 1)
+      coverage.list[[paste(tf.name, sample.id, experiment, strand)]] <-
+        data.frame(tf.name, sample.id, experiment, strand, one.join)
+    }
+  }
+
+  region.list[[tf.name]] <- pos.neg.regions
+  site.list[[tf.name]] <- data.frame(tf.name, nrsf.sites)
+}
+
+site.list2 <- list()
+region.list2 <- list()
+for(tf.name in names(site.list)){
+  site.list2[[tf.name]] <- site.list[[tf.name]] %>%
+    select(tf.name, region.index, chrom, chromStart, chromEnd, classification)
+  region.list2[[tf.name]] <- region.list[[tf.name]] %>%
+    select(tf.name, region.index, chrom, chromStart, chromEnd, annotation)
+}
+
+TF.benchmark <-
+  list(regions=do.call(rbind, region.list2),
+       sites=do.call(rbind, site.list2),
+       coverage=do.call(rbind, coverage.list))
+sapply(TF.benchmark, names)
+
+save(TF.benchmark, file="TF.benchmark.RData")
