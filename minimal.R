539d12bed45b412c8b8880e1a340e0a7f0556214
Toby Dylan Hocking <tdhock5@gmail.com>
2015-02-03 16:44:17 -0500

    nomatch=0

--- minimal.R
+++ minimal.R
@@ -25,7 +25,7 @@ GR_fun <- function(query, windows){
 require(data.table) #"Rdatatable/data.table@84ba1151299ba49e833e68a2436630216b653306"
 DT_fun <- function(query, windows){
   query.dt <- data.table(query)
-  setkey(query.dt, chrom, chromStart, chromEnd)
+  ##setkey(query.dt, chrom, chromStart, chromEnd)
   windows.dt <- data.table(windows)
   setkey(windows.dt, chrom, chromStart, chromEnd)
   foverlaps(query.dt, windows.dt, nomatch=0L, which=TRUE)
@@ -34,7 +34,7 @@ DT_fun <- function(query, windows){
 ## 5. so that I can call them as: GR_fun(query1, window); DT_fun(query1, window) etc..
 library(microbenchmark)
 microbenchmark(data.table={
-  foverlaps(chipseq.dt, windows)
+  foverlaps(chipseq.dt, windows, nomatch=0L, which=TRUE)
 }, data.table.setkey=DT_fun(chipseq.df, windows.df),
   GenomicRanges=GR_fun(chipseq.df, windows.df),
   times=2)
